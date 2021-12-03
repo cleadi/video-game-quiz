c7f01dbfa15e0b2f04ec5fb878244e1789880e45
Dillon Cleaver <cleadi@fastmail.com>
2021-12-02 18:09:09 -0600

    Initial commit.

---
+++ quiz-app/src/App.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import QuestionCard from "./components/QuestionCard";
+
+const App = () => {
+  const startTrivia = async () => {};
+
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
+
+  const nextQuestion = () => {};
+
+  return (
+    <div className="app">
+      <h1>Video Game Quiz</h1>
+      <button className="start" onClick="{startTrivia}">
+        Start
+      </button>
+      <p className="score">Score</p>
+      <p>Loading questions...</p>
+      <QuestionCard />
+      <button className="next" onClick={nextQuestion}>
+        Next Question
+      </button>
+    </div>
+  );
+};
+
+export default App;
