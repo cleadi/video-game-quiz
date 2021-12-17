355c7825ad830887b4fdc242691608a54d84e230
Dillon Cleaver <cleadi@fastmail.com>
2021-12-17 16:12:51 -0600

    Updates

---
+++ precache-manifest.735fd7a626eca038fe3840097cef96cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d78764d20efe906bc02e30acff5fd8b5",
+    "url": "/video-game-quiz/index.html"
+  },
+  {
+    "revision": "1d006e3451e956c4fb91",
+    "url": "/video-game-quiz/static/js/2.7020c005.chunk.js"
+  },
+  {
+    "revision": "2d0dc3ea56ea29aa89323d664321ba6d",
+    "url": "/video-game-quiz/static/js/2.7020c005.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12259843583678a48002",
+    "url": "/video-game-quiz/static/js/main.48a9b079.chunk.js"
+  },
+  {
+    "revision": "1c26092e1ef0843d7b01",
+    "url": "/video-game-quiz/static/js/runtime-main.a922b6b6.js"
+  },
+  {
+    "revision": "3225c99b6c5273dc01e395fe262ac67a",
+    "url": "/video-game-quiz/static/media/micha-frank.3225c99b.jpg"
+  }
+]);
